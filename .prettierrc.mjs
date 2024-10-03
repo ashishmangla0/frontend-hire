d543ed76e6501b6b92de3fd27772cdad44a38213
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-10-04 02:49:53 +0530

    add husky, tailwind

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,19 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
-  printWidth: 80,
-  tabWidth: 2,
-  semi: true,
-  jsxSingleQuote: true,
-  singleQuote: true,
-  useTabs: true,
-  trailingComma: "all",
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+	printWidth: 80,
+	tabWidth: 2,
+	semi: true,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	useTabs: true,
+	trailingComma: 'all',
 };
