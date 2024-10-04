b7aaf695281fe1ea4d73a73f3fde6bf3f97fa82e
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-10-04 15:35:02 +0530

    sharp and siteurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
+import { siteUrl } from '@/constants/site-config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: siteUrl,
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
