6eaade9ffe64892e09b7509e87c9d8820bbdaab3
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-10-12 23:23:39 +0530

    better schema

--- src/schema/question.ts
+++ src/schema/question.ts
@@ -8,9 +8,19 @@ const QuestionSkill = z.enum([
 	'next',
 	'dsa',
 ]);
-const QuestionType = z.enum(['coding', 'theory']);
-const CodingEnvironmentType = z.enum(['browser', 'local']);
-const Difficulty = z.enum(['easy', 'medium', 'hard', 'master']);
+
+export const QuestionType = z.enum(['coding', 'theory']);
+export const CodingEnvironmentType = z.enum(['browser', 'local']);
+export const Difficulty = z.enum(['easy', 'medium', 'hard', 'master']);
+
+const File = z.object({
+	code: z.string(),
+	hidden: z.boolean().optional(),
+	active: z.boolean().optional(),
+	readOnly: z.boolean().optional(),
+});
+
+const Template = z.enum(['static', 'react', 'vanilla']);
 
 // Define the base question schema
 const baseQuestionSchema = z.object({
@@ -28,11 +38,11 @@ const browserCodingQuestionSchema = baseQuestionSchema.extend({
 	type: z.literal(QuestionType.Enum.coding),
 	environment: z.literal(CodingEnvironmentType.Enum.browser),
 	recommendedLayout: z.enum(['col-3', 'col-2']),
-	template: z.string(), // Assuming SandpackPredefinedTemplate is a string
+	template: Template,
 	externalCDNs: z.array(z.string()).optional(),
 	showPreview: z.boolean().optional(),
 	showConsole: z.boolean().optional(),
-	files: z.record(z.string()), // Simplified representation of SandpackFiles
+	files: z.array(File).optional(),
 	dependencies: z.record(z.string()).optional(),
 });
 
