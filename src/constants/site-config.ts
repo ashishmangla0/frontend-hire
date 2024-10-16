67447681980069daad66468ed78aedee64b225be
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-10-16 20:51:33 +0530

    remove instagram

--- src/constants/site-config.ts
+++ src/constants/site-config.ts
@@ -88,25 +88,6 @@ const SITE_METADATA = {
 					</svg>`,
 		},
 		{
-			name: 'Instagram',
-			url: 'https://www.instagram.com/frontendhire/',
-			icon: `<svg
-						xmlns='http://www.w3.org/2000/svg'
-						stroke='currentColor'
-						fill='currentColor'
-						stroke-width='0'
-						class='h-6 w-6'
-						height='1em'
-						width='1em'
-						aria-hidden='true'
-						viewBox='0 0 256 256'
-					>
-						<path
-							d='M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z'
-						></path>
-					</svg>`,
-		},
-		{
 			name: 'YouTube',
 			url: 'https://www.youtube.com/@FrontendHire',
 			icon: `<svg
